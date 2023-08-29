31e59e0be12254f7da86ac85c09ede6d4fc51da4
Serhiy Chepa <schepa@pixelfederation.com>
2023-08-29 19:22:29 +0200

    [fix] initial delay a steps behaviour

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,61 +1,44 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const elements = {
-  form: document.querySelector('.form'),
-};
-
-const { delay, step, amount } = elements.form.elements;
-
-let promiseCounter = 1;
-let cumulativeDelay = 0; // variable to store cumulative response time (initial delay + step * number of steps)
-
-elements.form.addEventListener('submit', submitHandler);
+const form = document.querySelector('.form');
+const { delay, step, amount } = form.elements;
+form.addEventListener('submit', submitHandler);
 
 function submitHandler(evt) {
   evt.preventDefault();
-  cumulativeDelay = +delay.value;
-  // setting initial delay for the first promice
-  const timerId = setTimeout(() => {
-    //
-    const intervalId = setInterval(() => {
-      if (promiseCounter === +amount.value + 1) {
-        clearInterval(intervalId);
-        return;
-      }
-      const promise = createPromise(promiseCounter, cumulativeDelay);
-      promiseCounter++;
-      cumulativeDelay += +step.value;
+  let cumulativeDelay = +delay.value;
 
-      promise
-        .then(data => {
-          Notify.success(
-            `✅ Fulfilled promise ${data.position} in ${data.delay}ms`
-          );
-          return data;
-        })
-        .catch(err => {
-          Notify.failure(
-            `❌ Rejected promise ${err.position} in ${err.delay}ms`
-          );
-        });
-    }, +step.value);
-    // resetting counter of promises in case user wants to start again without refreshing the page
-    promiseCounter = 1;
-  }, +delay.value);
+  for (let i = 1; i <= +amount.value; i++) {
+    createPromise(i, cumulativeDelay)
+      .then(data => {
+        Notify.success(
+          `✅ Fulfilled promise ${data.position} in ${data.delay}ms`
+        );
+        return data;
+      })
+      .catch(err => {
+        Notify.failure(
+          `❌ Rejected promise ${err.position} in ${err.delay}ms`
+        );
+      })
+    cumulativeDelay += +step.value;
+  }
 }
 
 function createPromise(position, delay) {
   return new Promise((res, rej) => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      // Fulfill
-      res({ position, delay });
-      // console.log(shouldResolve);
-    } else {
-      // Reject
-      rej({ position, delay });
-      // console.log(shouldResolve);
-    }
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        res({ position, delay });
+        // console.log(shouldResolve);
+      } else {
+        // Reject
+        rej({ position, delay });
+        // console.log(shouldResolve);
+      }
+    }, delay)
   });
 }
 
@@ -64,4 +47,4 @@ Notify.init({
   width: '360px',
   fontSize: '16px',
   useIcon: false,
-});
+})
